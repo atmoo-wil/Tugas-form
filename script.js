facc94d5d744704ef8fcde250419ee11e3484e42
atmoo-wil <wilaga.atmo@gmail.com>
2022-10-15 19:34:39 +0700

    menambah card pada hasil form

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function nilaiMasuk() {
     const list = document.getElementById("list");
     console.log(nama);
 
-    list.innerHTML+=
-    `<li>
-        <p>${nama}</p>
-        <p>${email}</p>
-        <p>${telp}</p>
-        </li>`;
-}
-
+    document.getElementById("list-group").innerHTML+=
+    `
+    <ul class="list-group mt-4 ms-3" style="width: 30%; display:inline-block">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Nama: ${nama}
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Email: ${email}
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    No telp: ${telp}
+  </li>
+</ul>`
+};
